c9e8171ad1713db309e0eb1ed0db65a898874cff
Juanma <cursoredes@localhost.localdomain>
2021-06-07 21:03:32 +0200

    GitIgnore cambiado

---
+++ Client/Uno_Client.cc
@@ -0,0 +1,10 @@
+#include "Client.h"
+
+int main(int argc, char **argv) //argv[1] = direccion, argv[2] = puerto, argv[3] = nombre
+{
+    Client client(argv[1], argv[2], argv[3]);
+
+    client.StartGame();
+
+    client.Close();
+}
