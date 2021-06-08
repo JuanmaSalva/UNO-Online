557d14d05d8dc0d63c746eaed252902573ae86e7
Juanma <cursoredes@localhost.localdomain>
2021-06-08 19:06:38 +0200

    Detectan las conexiones y desconexiones pero el time wait sigue
    Co-authored-by: Esteban <35503563+EstebenR@users.noreply.github.com>

--- Client/Client.cc
+++ Client/Client.cc
@@ -3,12 +3,11 @@
 
 void Client::StartGame()
 {
-    //nos quedamos esperando en el recv a que nos digan que empiece el juego
-    std::cout << "Estamos a la espera\n";
-    ChatMessage msg;
-    //Socket *auxSock = &socket;
-    //while(socket.receive(msg, auxSock) != 0);
-    //std::cout << "El juego va a empezar\n";
+    while(socket.recv() != 0);
+
+    std::cout << "Nos han cerrado la conexión\n";
+
+    //Close();
 }
 
 void Client::Close(){
