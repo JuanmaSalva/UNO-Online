3a27ae7052801fa995ba916fc0261888caa3a8ef
Juanma <cursoredes@localhost.localdomain>
2021-06-10 21:20:14 +0200

    Nuevo mensaje Play
    Co-authored-by: Esteban <35503563+EstebenR@users.noreply.github.com>

---
+++ Common/Play.cc
@@ -0,0 +1,32 @@
+#include "Play.h"
+
+Play::Play(uint8_t card){
+	_size = sizeof(uint8_t);
+    cardPlayed = card;
+}
+
+void Play::to_bin()
+{
+	alloc_data(size());
+
+	memset(_data, 0, size());
+
+	char *tmp = _data;
+
+	memcpy(tmp, &cardPlayed, sizeof(uint8_t));
+	tmp += sizeof(uint8_t);
+}
+
+int Play::from_bin(char *data)
+{
+	alloc_data(size());
+
+	memcpy(static_cast<void *>(_data), data, size());
+
+	char *tmp = _data;
+
+	memcpy(&cardPlayed, tmp, sizeof(cardPlayed));
+	tmp += sizeof(cardPlayed);
+
+    return 0;
+}
